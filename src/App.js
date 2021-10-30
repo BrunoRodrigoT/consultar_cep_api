44b721d771dd546000c21e8b655be313aeda1e07
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-10-30 16:25:51 -0300

    api consultar cep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import api from './services/api';
+
 
 function App() {
+
+  const [cep, setCep] = useState('')
+  const [dados, setDados] = useState('')
+
+
+  function atribuirCep(e) {
+    e.preventDefault()
+    setCep(e.target.value)
+  }
+
+  function consultarCep(){
+    let url = cep + '/json'
+    api.get(url).then(
+      (response) => {
+        if(!response.data.erro){
+          let objeto = JSON.stringify(response.data)
+          setDados(objeto)
+        }else{
+          setDados('Dados não encontrados')
+        }
+      }
+      ).catch(
+      (err) => {
+        setDados('Dados não encontrados')
+      }
+    )
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Consultar CEP</h1>
+      <div>
+        <input type='number' name='cep' onChange={(e) => atribuirCep(e)}></input>
+        <button onClick={() => consultarCep()}>Buscar Cep</button>
+      </div>
+      <div name='resultado'>
+        {dados}
+      </div>
+    </>
   );
 }
 
