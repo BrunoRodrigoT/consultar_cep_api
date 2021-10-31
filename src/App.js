4d94b3e93cb4153e22df2c2cbf62f7b98e51f9ee
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-10-31 09:52:14 -0300

    atualizations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, {useState} from 'react';
 import './App.css';
 import api from './services/api';
+import icon from './img/seta.svg'
 
 
 function App() {
 
   const [cep, setCep] = useState('')
   const [dados, setDados] = useState('')
-
+  
+  const listar = dados.map((dado, value) => {
+    return <li key={value}>{dado}</li>
+  })
 
   function atribuirCep(e) {
     e.preventDefault()
@@ -33,17 +38,20 @@ function App() {
 
   }
 
+
   return (
-    <>
-      <h1>Consultar CEP</h1>
-      <div>
-        <input type='number' name='cep' onChange={(e) => atribuirCep(e)}></input>
-        <button onClick={() => consultarCep()}>Buscar Cep</button>
+    <div id='box'>
+      <h1>Consultar <span id='span'>CEP</span></h1>
+      <div id='inputs'>
+        <input type='tel' name='cep' onChange={(e) => atribuirCep(e)}></input>
+        <button onClick={() => consultarCep()}><img src={icon} id='icon'></img></button>
       </div>
-      <div name='resultado'>
-        {dados}
+      <div id='resultado'>
+        <ul>
+          {listar}
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
